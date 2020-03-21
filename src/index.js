34976a282d66c04b82fd98d460251cc74ab5706f
Rodolfo Roman <rromanv@gmail.com>
2020-03-21 15:36:43 -0500

    Added Error Handlers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 import helmet from 'helmet'
 import pino from 'express-pino-logger'
 
+import { notFound, errorHandler } from './helpers/errors'
+
 const app = express()
 const port = parseInt(process.env.PORT)
 
@@ -13,8 +15,11 @@ app.use(helmet())
 app.use(pino())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello There' })
+app.use('/', (req, res) => {
+  res.json({ msg: 'Hello There', type: req.method })
 })
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port)
