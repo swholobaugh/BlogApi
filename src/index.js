c2550e2c699e36ee24c6f873e6c3b64f58aae193
Rodolfo Roman <rromanv@gmail.com>
2020-03-21 15:27:12 -0500

    added packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import './helpers/dotenv'
 
 import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import pino from 'express-pino-logger'
 
 const app = express()
 const port = parseInt(process.env.PORT)
+
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
+app.use(pino())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Hello There' })
